641de47550d2063c01eed2b416b031bac85472b6
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-01 18:46:17 +0900

    null

--- Watcha/src/common/handler/NullHandler.java
+++ Watcha/src/common/handler/NullHandler.java
@@ -1,5 +1,13 @@
 package common.handler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class NullHandler implements CommandHandler {
 
+	public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+		resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+		return null;
+	}
+	
 }
