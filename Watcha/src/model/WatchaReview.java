5e4a6d5f31319410b7ce0f4c024108fdff31555e
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-01 19:48:26 +0900

    yeah

--- Watcha/src/model/WatchaReview.java
+++ Watcha/src/model/WatchaReview.java
@@ -1,5 +1,72 @@
 package model;
 
-public class WatchaReview {
+import java.time.LocalDateTime;
 
+public class WatchaReview {
+	private int reviewId;
+	private Member member;
+	private int star;
+	private String review;
+	private LocalDateTime wdate;
+	private LocalDateTime udate;
+	public int getReviewId() {
+		return reviewId;
+	}
+	//생성자 1. DB에서 가져올 때 사용할 생성자
+	public WatchaReview(int reviewId, Member member, int star, String review, LocalDateTime wdate,
+			LocalDateTime udate) {
+		super();
+		this.reviewId = reviewId;
+		this.member = member;
+		this.star = star;
+		this.review = review;
+		this.wdate = wdate;
+		this.udate = udate;
+	}
+	
+	public WatchaReview(int reviewId, Member member, int star, String review) {
+		
+	}
+	
+	
+	//getter, setter
+	public void setReviewId(int reviewId) {
+		this.reviewId = reviewId;
+	}
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public LocalDateTime getWdate() {
+		return wdate;
+	}
+	public void setWdate(LocalDateTime wdate) {
+		this.wdate = wdate;
+	}
+	public LocalDateTime getUdate() {
+		return udate;
+	}
+	public void setUdate(LocalDateTime udate) {
+		this.udate = udate;
+	}
+
+
+
+	
+	
+	
 }
