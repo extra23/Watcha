70dd81d070737ca4d3eadd465880ac2ced9d9d8f
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-10-01 19:50:45 +0900

    Movie Genre, Pre

--- Watcha/src/model/MovieGenre.java
+++ Watcha/src/model/MovieGenre.java
@@ -1,5 +1,28 @@
 package model;
 
 public class MovieGenre {
+	private int genreId;
+	private String genreName;
+	
+	public MovieGenre(int genreId, String genreName) {
+		this.genreId = genreId;
+		this.genreName = genreName;
+	}
 
+	public int getGenreId() {
+		return genreId;
+	}
+
+	public void setGenreId(int genreId) {
+		this.genreId = genreId;
+	}
+
+	public String getGenreName() {
+		return genreName;
+	}
+
+	public void setGenreName(String genreName) {
+		this.genreName = genreName;
+	}
+	
 }
--- Watcha/src/model/MoviePre.java
+++ Watcha/src/model/MoviePre.java
@@ -1,5 +1,66 @@
 package model;
 
-public class MoviePre {
+import java.time.LocalDateTime;
 
+public class MoviePre {
+	private int movieId;
+	private String title;
+	private int time;
+	private LocalDateTime releaseDate;
+	private int rate;
+	private String shortPlot;
+	private String image;
+	public MoviePre(int movieId, String title, int time, LocalDateTime releaseDate, int rate, String shortPlot,
+			String image) {
+		this.movieId = movieId;
+		this.title = title;
+		this.time = time;
+		this.releaseDate = releaseDate;
+		this.rate = rate;
+		this.shortPlot = shortPlot;
+		this.image = image;
+	}
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public LocalDateTime getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(LocalDateTime releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public int getRate() {
+		return rate;
+	}
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	public String getShortPlot() {
+		return shortPlot;
+	}
+	public void setShortPlot(String shortPlot) {
+		this.shortPlot = shortPlot;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
 }
