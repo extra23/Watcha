c43126b7bcbf4761cf606a81883e48fc7e509e14
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-01 21:49:46 +0900

    erasdf

--- Watcha/src/handler/account/ModifyAccountHandler.java
+++ Watcha/src/handler/account/ModifyAccountHandler.java
@@ -1,5 +1,23 @@
 package handler.account;
 
-public class ModifyAccountHandler {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import common.handler.CommandHandler;
+
+public class ModifyAccountHandler implements CommandHandler{
+
+	//수정할 페이지 생성
+	private static final String FORM_VIEW = "/WEB-INF/view/";
+
+	
+	//입력받은 데이터(비밀번호)가 문제가 있는지 무결성 체크 후 비번화면으로 돌려보내거나
+	//정보 수정 후 성공화면으로 보낸다.
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		
+		return null;
+	}
+
+	
 }
--- Watcha/src/service/account/ModifyAccountService.java
+++ Watcha/src/service/account/ModifyAccountService.java
@@ -1,5 +1,58 @@
 package service.account;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import jdbc.ConnectionProvider;
+import model.Member;
+
 public class ModifyAccountService {
+
+	// 싱글톤
+	private static ModifyAccountService instance = new ModifyAccountService();
+
+	public static ModifyAccountService getInstance() {
+		return instance;
+	}
+
+	private ModifyAccountService() {
+	}
+
+	// 비밀번호 변경위해 비즈니스 로직 수행
+	// 로그인 아이디, 현 비번, 새 비번을 인자로 받고 그것을 통해서 비번이 제대로 되었는지
+	// 현재 있는 사용자인지 확인하고 로직을 수행한다.
+	public void changePassword(String userId, String oldPwd, String newPwd) {
+		/*MemberDao memberDao = MemberDao.getInstance();
+		try (Connection conn = ConnectionProvider.getConnection()) {
+
+			try {
+				conn.setAutoCommit(false);
+				// user객체를 받아오고
+
+				Member member = memberDao.selectByLoginId(conn, userId);
+
+				// 없는 유저라면
+				if (member == null) {
+					throw new MemberNotFoundException("없는 멤버");
+				}
+
+				// member객체와 입력받은 비밀번호를 비교해서 잘못된 비밀번호를 입력했다면
+				if (!member.matchPassword(oldPwd)) {
+					throw new InvalidPasswordException("잘못된 비밀번호");
+				}*/
+				// 업데이트에 보낼 객체의 비번을 새로 넣어줌
+				// 정상이면 update
+			/*	member.setPassword(newPwd);
+				memberDao.update(conn, member);
+				conn.commit();
+			} catch (SQLException e) {
+				// 아니면 예외날리고 롤백
+				conn.rollback();
+				throw new RuntimeException(e);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}*/
+	}
 
 }
