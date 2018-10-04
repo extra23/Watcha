5acafcdfd70d3d508580d027b9612c18b560ce4b
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-04 19:57:36 +0900

    MovieData

--- Watcha/src/service/account/AccountData.java
+++ Watcha/src/service/account/AccountData.java
@@ -1,34 +1,5 @@
 package service.account;
 
-import model.MovieDetail;
-import model.MoviePre;
-
-//게시글 보기에 필요한 객체
 public class AccountData {
-	
-	//moviePre 테이블 내용
-	private MoviePre moviePre;
-	//MovieDetail 테이블 내용
-	private MovieDetail movieDetail;
-	
-	//constructor
-	public AccountData(MoviePre moviePre, MovieDetail movieDetail) {
-		this.moviePre = moviePre;
-		this.movieDetail = movieDetail;
-	}
 
-	//getter
-	public MoviePre getMoviePre() {
-		return moviePre;
-	}
-
-	public MovieDetail getMovieDetail() {
-		return movieDetail;
-	}
-	
-	
-	
-
-	
-	
 }
--- Watcha/src/service/movie/MovieData.java
+++ Watcha/src/service/movie/MovieData.java
@@ -1,5 +1,27 @@
 package service.movie;
 
-public class MovieData {
+import model.MovieDetail;
+import model.MoviePre;
 
+public class MovieData {
+	//moviePre 테이블 내용
+		private MoviePre moviePre;
+		//MovieDetail 테이블 내용
+		private MovieDetail movieDetail;
+		
+		//constructor
+		public MovieData(MoviePre moviePre, MovieDetail movieDetail) {
+			this.moviePre = moviePre;
+			this.movieDetail = movieDetail;
+		}
+
+		//getter
+		public MoviePre getMoviePre() {
+			return moviePre;
+		}
+
+		public MovieDetail getMovieDetail() {
+			return movieDetail;
+		}
+		
 }
