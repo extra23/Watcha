1bcffd63d3fc346576668f2da7f925d6206ce814
KOITT-02-A <KOITT-02-A@KOITT-02-A-PC>
2018-10-04 21:03:53 +0900

    readHandler

--- Watcha/src/handler/movie/ReadMovieHandler.java
+++ Watcha/src/handler/movie/ReadMovieHandler.java
@@ -1,5 +1,30 @@
 package handler.movie;
 
-public class ReadMovieHandler {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import common.exception.ArticleContentNotFoundException;
+import common.exception.ArticleNotFoundException;
+import common.handler.CommandHandler;
+import service.movie.MovieData;
+import service.movie.ReadMovieService;
+
+public class ReadMovieHandler implements CommandHandler{
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// 사용자에게 요청을 받고 서비스를 이용해서 화면에 보여줄 데이터 생성
+		int movieId = Integer.parseInt(req.getParameter("no"));
+		ReadMovieService movieService = ReadMovieService.getInstance( );
+		
+		// 화면으로 리턴
+		try {
+			MovieData movieData = movieService.getMovie(movieId, true);
+			req.setAttribute("movieData", movieData);
+			
+			return "/WEB-INF/view/movie/movie_list.jsp";
+		}catch(ArticleNotFoundException | ArticleContentNotFoundException e) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return null;
+	}
 }
