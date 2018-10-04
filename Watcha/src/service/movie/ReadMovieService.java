acdf4eadc88fa913bfd545a6a20ba10c70d9f0e1
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-04 21:42:02 +0900

    readMovieService

--- Watcha/src/service/movie/ReadMovieService.java
+++ Watcha/src/service/movie/ReadMovieService.java
@@ -1,5 +1,51 @@
 package service.movie;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import Exception.MovieDetailNotFoundException;
+import Exception.MoviePreNotFoundException;
+import dao.MovieDetailDAO;
+import dao.MoviePreDAO;
+import jdbc.ConnectionProvider;
+import model.MovieDetail;
+import model.MoviePre;
+
 public class ReadMovieService {
+
+	//싱글톤
+	private static ReadMovieService instance = new ReadMovieService();
+	private ReadMovieService() {
+	}
+	public static ReadMovieService getInstance() {
+		return instance;
+	}
+
+	//한 편의 movie를 읽어오는 메소드
+	public MovieData getMovie(int movieId) {
+
+		MoviePreDAO moviePreDAO = MoviePreDAO.getInstance();
+		MovieDetailDAO movieDetailDAO = MovieDetailDAO.getInstance();
+
+		try (Connection conn = ConnectionProvider.getConnection()) {
+			MoviePre moviePre = moviePreDAO.selectById(conn, movieId);
+			
+			if (moviePre == null) {
+				throw new MoviePreNotFoundException("moviePre이 없음");
+			}
+
+			MovieDetail movieDetail = movieDetailDAO.selectById(conn, movieId);
+			
+			if (movieDetail ==null) {
+				throw new MovieDetailNotFoundException("movieDetail이 없음");
+			}
+
+			return new MovieData(moviePre, movieDetail);
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 
 }
