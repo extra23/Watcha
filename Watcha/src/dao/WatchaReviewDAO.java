e51827989dfd8069b3c783cb4643161462fb6e8b
KOITT_Kim <KOITT_P@KOITT_P-PC>
2018-10-06 16:04:39 +0900

    reviewService, reviewDao watchaReview수정

--- Watcha/src/dao/WatchaReviewDAO.java
+++ Watcha/src/dao/WatchaReviewDAO.java
@@ -2,39 +2,98 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.WatchaReview;
 import service.review.ReviewRequest;
 
-
-
 public class WatchaReviewDAO {
-	
-	//싱글톤
+
+	// 싱글톤
 	private static WatchaReviewDAO instance = new WatchaReviewDAO();
-	public  static WatchaReviewDAO getInstance() {
+
+	public static WatchaReviewDAO getInstance() {
 		return instance;
 	}
-	private WatchaReviewDAO () {}
-	
-	//사용자 리뷰 추가
+
+	private WatchaReviewDAO() {
+	}
+
+	// 사용자 리뷰 추가
 	public int insert(Connection conn, ReviewRequest reviewRequest) throws SQLException {
 		String sql = "insert into watcha_review(member_id,star,review) values(?,?,?)";
-		try(PreparedStatement pst = conn.prepareStatement(sql)){
+		try (PreparedStatement pst = conn.prepareStatement(sql)) {
 			pst.setInt(1, reviewRequest.getMemberId());
 			pst.setInt(2, reviewRequest.getStar());
 			pst.setString(3, reviewRequest.getReview());
 			return pst.executeUpdate();
 		}
 	}
+
+	// watcha_review의 tuple수를 가져오는 메소드
+	public int selectCount(Connection conn) throws SQLException {
+		String sql = "select count(*) from watcha_review";
+		try (Statement st = conn.createStatement(); ResultSet rs = st.executeQuery(sql)) {
+			if (rs.next()) {
+				return rs.getInt(1);
+			}
+		}
+		return 0;
+	}
+
+	// 리미트를 이용한 리스트를 가져오는 쿼리
+	public List<WatchaReview> select(Connection conn, int StartRow, int size) throws SQLException {
+		String sql = "select * from watcha_review order by review_id limit ?,?";
+		try (PreparedStatement pst = conn.prepareStatement(sql)) {
+			pst.setInt(1, StartRow);
+			pst.setInt(2, size);
+			try (ResultSet rs = pst.executeQuery()) {
+				List<WatchaReview> reviewList = new ArrayList<WatchaReview>();
+				while (rs.next()) {
+					reviewList.add(convReview(rs));
+				}
+				return reviewList;
+
+			}
+		}
+	}
 	
-	//사용자 리뷰 삭제
+	//review_id로 특정 리뷰를 가져오는 메소드
+	public WatchaReview selectById(Connection conn, int reviewId) throws SQLException {
+		String sql = "select * from watcha_review where review_id = ?";
+		try(PreparedStatement pst = conn.prepareStatement(sql)){
+			pst.setInt(1, reviewId);
+			try(ResultSet rs = pst.executeQuery()){
+				WatchaReview watchaReview = null;
+				if(rs.next()) {
+					watchaReview = convReview(rs);
+				}
+				return watchaReview;
+			}
+		}
+	}
+
+	// 사용자 리뷰 삭제
 	public int delete(Connection conn, int memberId) throws SQLException {
 		String sql = "delete from watcha_review where member_id =?";
-		try(PreparedStatement pst = conn.prepareStatement(sql)){
+		try (PreparedStatement pst = conn.prepareStatement(sql)) {
 			pst.setInt(1, memberId);
 			return pst.executeUpdate();
 		}
+	}
+	
+
+	//ResultSet으로 나온결과를 Watcha_review객체로 생성해서 담는 메소드
+	private WatchaReview convReview(ResultSet rs) throws SQLException {
+		
+		WatchaReview watchaReview = new WatchaReview(rs.getInt("review_id"),
+									rs.getInt("member_id"),
+									rs.getInt("star"), 
+									rs.getString("review"));
+		return watchaReview;
 	}
 }
--- Watcha/src/service/review/ListReviewService.java
+++ Watcha/src/service/review/ListReviewService.java
@@ -1,5 +1,13 @@
 package service.review;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import dao.WatchaReviewDAO;
+import jdbc.ConnectionProvider;
+import model.WatchaReview;
+
 public class ListReviewService {
 
 	//싱글톤
@@ -12,5 +20,14 @@ public class ListReviewService {
 	private int size = 3; //한 페이지에 보여줄 리뷰 개수
 	private int blockSize = 5; //한 페이지에서 보여줄 하단 페이지 링크의 개수
 	
-	
+	public ReviewPage getReviewPage(int pageNum) {
+		try(Connection conn = ConnectionProvider.getConnection()){
+			WatchaReviewDAO watchaReviewDAO = WatchaReviewDAO.getInstance();
+			int total = watchaReviewDAO.selectCount(conn);
+			List<WatchaReview> reviewList = watchaReviewDAO.select(conn, (pageNum-1)*size, size);
+			return new ReviewPage(reviewList, pageNum, total, size, blockSize);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
